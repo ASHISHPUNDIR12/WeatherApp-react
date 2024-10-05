a99435009f0eb8e870c7ca141fcafcdf084a22da
ASHISHPUNDIR12 <ashishpundir1012@gmail.com>
2024-10-05 20:47:23 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/WeatherApp-react/', // Replace 'your-repo-name' with your GitHub repository name
+});
